020590a74eca090bec963f78a9ecbd0fd70ce9e7
timschro <tim.s@hey.com>
2020-12-06 00:24:56 +0100

    further refactoring and simplifying

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,35 +1,11 @@
 # https://adventofcode.com/2020/day/5
-
-$ids = []
+ids = []
 REPLACEMENTS = {'F' => '0', 'B' => '1', 'R' => '1', 'L' => '0'}
-
-
-def check_boardingpasses(passes)
-  passes.each do |pass|
-    pass.gsub!(Regexp.union(REPLACEMENTS.keys), REPLACEMENTS)
-    $ids << pass[0,7].to_i(2) * 8 + pass[7,4].to_i(2)
-  end
-  $ids.max
+File.readlines("input.txt").each do |pass|
+  pass.gsub!(Regexp.union(REPLACEMENTS.keys), REPLACEMENTS)
+  ids << pass[0,7].to_i(2) * 8 + pass[7,4].to_i(2)
 end
+seat = ids.min.upto(ids.max).select {|i| !ids.include?(i)}
 
-def find_emptyseat
-  $ids.min.upto($ids.max) {|i| return i unless $ids.include?(i)}
-end
-
-
-if $0 == __FILE__
- puts "Result Part 1: #{check_boardingpasses(File.readlines("input.txt"))}"
- puts "Result Part 2: #{find_emptyseat}"
-end
-
-
-########## RSpec tests
-# Run tests: ´rspec day5.rb´
-
-require "rspec"
-
-RSpec.describe "Day 5: Binary Boarding" do
-  it "Highest ID is 820" do
-    expect(check_boardingpasses(File.readlines("test.txt"))).to eq(820)
-  end
-end
+puts "Result Part 1: #{ids.max}"
+puts "Result Part 2: #{seat[0]}"
